c623bb06cb7f8b1e98a2c30c097ecb57d0fdb6ee
Osarieme Naomi Osunde <naomiosariemeosunde@gmail.com>
2023-01-31 23:52:28 +0100

    Update 100-textme.rb

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,10 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(//).join
+#Script matches a regular expression
+
+output = ARGV[0].scan(/from:([^\]]*\b)|to:([^\]]*\b)|flags:([^\]]*\b)/).concat()
+all = []
+output.each do |i|
+  all.concat(i)
+end
+all.select! {|i| i != nil}
+puts all.join(",")
