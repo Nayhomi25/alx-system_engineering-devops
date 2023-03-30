b0c4aa0a7e003ed6a7c51ca5e55eb3af8bd4ccfb
Osunde Osarieme Naomi <naomiosariemeosunde@gmail.com>
2023-03-29 18:29:48 -0700

    I love puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,8 @@
-# This puppet manifest creates the file school in /tmp
-
-file { 'school':
-  ensure  => file,
-  path    => '/tmp/school',
-  mode    => '0744',
-  owner   => 'www.data',
-  group   => 'www.data',
-  content => 'I love Puppet'
+# This manifest creates a file at /tmp
+file { '/tmp/school':
+    ensure  => 'file',
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0744',
+    content => 'I love Puppet',
 }
