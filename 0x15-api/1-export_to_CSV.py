da823105169ff97e8fcde4d33cb95db9314abf08
Osunde Osarieme Naomi <naomiosariemeosunde@gmail.com>
2023-09-25 13:39:53 -0700

    fixing bugs

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,34 +1,24 @@
 #!/usr/bin/python3
-"""script for parsing web data from an api
-"""
-if __name__ == "__main__":
-    import json
-    import requests
-    import sys
-    base_url = 'https://jsonplaceholder.typicode.com/'
-    try:
-        employee_id = sys.argv[1]
-    except:
-        print('Usage: {} employee_id'.format(sys.argv[0]))
-        exit(1)
+"""Accessing a REST API for todo lists of employees"""
 
-    # grab the info about the user
-    url = base_url + 'users?id={}'.format(employee_id)
-    response = requests.get(url)
-    user = json.loads(response.text)
-    user_name = user[0].get('username')
+import requests
+import sys
 
-    # grab the info about the user's tasks
-    url = base_url + 'todos?userId={}'.format(employee_id)
+
+if __name__ == '__main__':
+    employeeId = sys.argv[1]
+    baseUrl = "https://jsonplaceholder.typicode.com/users"
+    url = baseUrl + "/" + employeeId
+
     response = requests.get(url)
-    objs = json.loads(response.text)
-    builder = ""
-    for obj in objs:
-            builder += '"{}","{}","{}","{}"\n'.format(
-                employee_id,
-                user_name,
-                obj.get('completed'),
-                obj.get('title')
-            )
-    with open('{}.csv'.format(employee_id), 'w') as myFile:
-        myFile.write(builder)
+    username = response.json().get('username')
+
+    todoUrl = url + "/todos"
+    response = requests.get(todoUrl)
+    tasks = response.json()
+
+    with open('{}.csv'.format(employeeId), 'w') as file:
+        for task in tasks:
+            file.write('"{}","{}","{}","{}"\n'
+                       .format(employeeId, username, task.get('completed'),
+                               task.get('title')))
